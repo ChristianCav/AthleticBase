78758fb6f750c2a439179d616d0a9d90dfc44c97
ChristianCav <christiancav55@gmail.com>
2025-07-21 13:04:31 -0400

    signup page aesthetic completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,14 +7,14 @@ const Navbar = () => {
         <div className="navbar-content flex-1">
             <Link to="/" className='flex items-center gap-1'>
                 <img src="/AthleticBaseLogo.png" alt="AthleticBase Logo" className='w-14 sm:w-16 md:w-18 lg:w-20 xl:w-22 h-auto object-contain' />
-                <span className='text-3xl text-gray-200 font-semibold'>AthleticBase</span>
+                <span className='text-3xl text-white font-semibold'>AthleticBase</span>
             </Link>
         </div>
         <div className="flex-none space-x-2">
-            <Link to="/login" className='btn btn-ghost text-lg text-gray-200'>
+            <Link to="/login" className='btn btn-ghost text-lg text-white'>
                 Login
             </Link>
-            <Link to="/signup" className='btn btn-ghost text-lg text-gray-200'>
+            <Link to="/signup" className='btn btn-ghost text-lg text-white'>
                 Sign Up
             </Link>
         </div>
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -2,8 +2,46 @@ import React from 'react'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+      <div className="min-h-screen flex items-center justify-center">
+      <div className="absolute top-0 -z-10 inset-0 bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+        <div className="card w-full mt-2 max-w-md bg-neutral-850" style={{ boxShadow: '0 10px 25px rgba(255, 255, 255, 0.5)' }}>
+          <div className="card-body text-white">
+            <h2 className="card-title justify-center">Sign Up</h2>
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="Enter your email"
+                className="input input-bordered"
+              />
+            </div>
+
+            <div className="form-control mt-4">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="Create a password"
+                className="input input-bordered"
+              />
+            </div>
+
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Create Account</button>
+            </div>
+          </div>
+        </div>
+      </div>
   )
 }
 
 export default SignUp
+
+
+
+
+
