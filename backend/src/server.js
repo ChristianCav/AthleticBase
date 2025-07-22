4c4ab06dc2d0b1f432b1c8d956a065047f439e16
ChristianCav <christiancav55@gmail.com>
2025-07-22 00:04:39 -0400

    signup functionality complete

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import performancesRoutes from "./routes/performancesRoutes.js"
 import usersRoutes from "./routes/usersRoutes.js"
 import { connectDB } from "./config/db.js";
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ const PORT = process.env.PORT || 5000
 
 // middleware
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/performances", performancesRoutes);
 app.use("/api/users", usersRoutes);
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router'
 
 const Navbar = () => {
   return (
-    <div className='navbar fixed left-0 top-0 z-50 border-b backdrop-blur-sm border-white/20 bg-neutral-950/30'>
+    <div className='navbar fixed left-0 max-h-20 top-0 z-50 border-b backdrop-blur-sm border-white/20 bg-neutral-950/30'>
         <div className="navbar-content flex-1">
             <Link to="/" className='flex items-center gap-1'>
                 <img src="/AthleticBaseLogo.png" alt="AthleticBase Logo" className='w-14 sm:w-16 md:w-18 lg:w-20 xl:w-22 h-auto object-contain' />
